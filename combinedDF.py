7727ce84e4cf18fd182cf168fd9368a649b4d6e0
bryann98 <47532133+bryann98@users.noreply.github.com>
2021-11-06 13:58:14 -0400

    Made new merge csv into batches script

--- combinedDF.py
+++ combinedDF.py
@@ -1,52 +1,65 @@
-# import bs4
-import zipfile
-import requests
 # import numpy
-# import pandas as pd
 import os
-import pickle
 # from io import BytesIO
 # from zipfile import ZipFile
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import os
 import glob
 import pandas as pd
+import os
+import datetime
+import fnmatch
+import csv
+from datetime import datetime, timedelta
+dirpath = r'C:\Users\thela\Downloads\AntWebScrape' # Change to desired directory
+os.chdir(dirpath)
+csv_list = []
+# Convert current time to seconds
+currDate = datetime.today() 
+currSec = currDate.timestamp()
+# For each csv file in directory containing raw data  
+for file_name in glob.glob(os.path.join(dirpath, '*.csv')):
+    if fnmatch.fnmatch(file_name, '*puf*') and not "repwgt" in file_name:
+        # Get time the file was modified
+        fileSec = os.path.getmtime(file_name)
+        diffTime = currSec - fileSec
+        # If file was modified within last 24 hours
+        if diffTime <= 86400:
+            print(file_name)
+        # Add file name to list of csv files
+        csv_list.append(file_name)
 
 # If directory does not exist yet, make it
 comPath = r'C:\Users\thela\Downloads\AntWebScrapeCombined' # Change to desired directory
 if not os.path.exists(comPath): 
      os.makedirs(comPath)
 
-import fnmatch
-csv_list = []
-for file_name  in glob.glob(os.path.join(dirpath, '*.csv')):
-    if fnmatch.fnmatch(file_name, '*puf*') and not "repwgt" in file_name:
-        csv_list.append(file_name)
+from itertools import chain
 os.chdir(comPath)
-#combined_csv = pd.concat([pd.read_csv(f) for f in csv_list ])
-#combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
+csv_phase1 = list(chain(csv_list[0:11]))
+combined_csv = pd.concat([pd.read_csv(f) for f in csv_phase1 ])
+combined_csv.to_csv( "combined_1.csv", index=False, encoding='utf-8-sig')
 
-import csv
-fieldnames = []
-# First determine the field names from the top line of each input file
-for filename in csv_list:
-  with open(filename, "r", newline="") as f_in:
-    reader = csv.reader(f_in)
-    headers = next(reader)
-    for h in headers:
-      if h not in fieldnames:
-        fieldnames.append(h)
 
-# Then copy the data
-with open("out.csv", "w", newline="") as f_out:
-  writer = csv.DictWriter(f_out, fieldnames=fieldnames)
-  for filename in csv_list:
-    with open(filename, "r", newline="") as f_in:
-        # Uses the field names in this file
-        reader = csv.DictReader(f_in)  
-        for line in reader:
-            writer.writerow(line)
+csv_phase2 = list(chain(csv_list[12:16]))
+combined_csv = pd.concat([pd.read_csv(f) for f in csv_phase2 ])
+combined_csv.to_csv( "combined_2.csv", index=False, encoding='utf-8-sig')
 
-df = pd.read_csv ('out.csv')
-print(df)
+
+csv_phase3 = list(chain(csv_list[17:26]))
+combined_csv = pd.concat([pd.read_csv(f) for f in csv_phase3 ])
+combined_csv.to_csv( "combined_3.csv", index=False, encoding='utf-8-sig')
+
+
+csv_phase31 = list(chain(csv_list[27:32]))
+combined_csv = pd.concat([pd.read_csv(f) for f in csv_phase31 ])
+combined_csv.to_csv( "combined_31.csv", index=False, encoding='utf-8-sig')
+
+
+csv_phase32 = list(chain(csv_list[33:38]))
+combined_csv = pd.concat([pd.read_csv(f) for f in csv_phase32 ])
+combined_csv.to_csv( "combined_32.csv", index=False, encoding='utf-8-sig')
+
+df1 = pd.read_csv("combined_1.csv") 
+df2 = pd.read_csv("combined_2.csv") 
+df3 = pd.read_csv("combined_3.csv") 
+df31 = pd.read_csv("combined_31.csv") 
+df32 = pd.read_csv("combined_32.csv")
