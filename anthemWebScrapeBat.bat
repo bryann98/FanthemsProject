4f16a5509058f58dbfe775e762fcb529997dbb1a
bryann98 <47532133+bryann98@users.noreply.github.com>
2021-12-14 15:28:51 -0500

    Deleted batch file that is not required

--- anthemWebScrapeBat.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-Rem Change to desired file paths to run script
-SET theExe = "C:\Users\thela\anaconda3\python.exe"
-SET thePy = "C:\Users\thela\Downloads\anthemWebScrapeTest4.py"
-"C:\Users\thela\anaconda3\python.exe" "C:\Users\thela\Downloads\anthemWebScrapeTest4.py" %*
-pause
